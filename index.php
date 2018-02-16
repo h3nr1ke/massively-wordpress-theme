88d4d46f3b2d3b6f7efb91b7f9533be56e6e7448
h3nr1ke <francisco.deodato@tvfsoftware.com>
2018-02-16 19:26:25 -0200

    Initial theme structure

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+  * Massively Wordpress Theme
+  *
+  * This is a wordpress theme based on the project 
+  * @see https://html5up.net/
+  *
+  * in specific the theme
+  * @see https://html5up.net/massively
+  *
+  * @link https://codex.wordpress.org/Template_Hierarchy
+  *
+  * @package WordPress
+  * @subpackage Appsetc_Massively_Theme
+  * @since 0.1
+  * @version 0.1
+  * 
+  */
