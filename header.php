360bdeff31c0e03b7d371eff95ef3975b67f8524
h3nr1ke <francisco.deodato@tvfsoftware.com>
2018-02-17 23:32:46 -0300

    1 - added Massively theme source files 2 - added theme file based in the instructions on     https://developer.wordpress.org/themes/getting-started

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<!--
+  Massively by HTML5 UP
+  html5up.net | @ajlkn
+  Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+  <head>
+    <title>Massively by HTML5 UP</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <link rel="stylesheet" href="assets/css/main.css" />
+    <noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
+    <?php wp_head(); ?>
+  </head>
+  <body class="is-loading">
+    <!-- Wrapper -->
+      <div id="wrapper" class="fade-in">
