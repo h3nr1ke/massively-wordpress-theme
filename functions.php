689802956bf5371477e2fbdaa99a1dd37b7bc72e
h3nr1ke <francisco.deodato@tvfsoftware.com>
2018-02-18 00:04:13 -0300

    based in the twentyseventeen wp theme, some customization tags were included. For instance, the <title> tag will be handled by wordpress directly, there is no need to include it in the code (it was removed from heade.php)

--- functions.php
+++ functions.php
@@ -1,4 +1,38 @@
 <?php
+/**
+ * A lot of code here were based in the twentyseventeen wordpress theme
+ * @see  https://core.trac.wordpress.org/browser/trunk/src/wp-content/themes/twentyseventeen?order=name
+ */
+
+/*
+ * Make theme available for translation.
+ */
+load_theme_textdomain( 'appsetc-massively' );
+
+/*
+ * Let WordPress manage the document title.
+ * By adding theme support, we declare that this theme does not use a
+ * hard-coded <title> tag in the document head, and expect WordPress to
+ * provide it for us.
+ * @see https://core.trac.wordpress.org/browser/trunk/src/wp-content/themes/twentyseventeen/functions.php
+ */
+add_theme_support( 'title-tag' );
+
+/*
+ * Enable support for Post Thumbnails on posts and pages.
+ * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+ */
+add_theme_support( 'post-thumbnails' );
+
+/**
+ * Based in the images available in the theme 
+ */
+add_image_size( 'appsetc-massively-featured-image', 896, 436, true );
+add_image_size( 'appsetc-massively-thumbnail', 448, 273, true );
+
+/**
+ * Enqueue all the need files to be included by wordpress engine
+ */
 function appsetc_massively_scripts() {
   // add the theme scripts
   wp_enqueue_script( 'appsetc-massively-scrollex', get_theme_file_uri( '/assets/js/jquery.scrollex.min.js' ), array( 'jquery' ), '1.0', true );
--- header.php
+++ header.php
@@ -6,7 +6,6 @@
 -->
 <html>
   <head>
-    <title>Massively by HTML5 UP</title>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
     <link rel="stylesheet" href="assets/css/main.css" />
