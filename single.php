830914b736f1da0553df3082a69225591762171a
h3nr1ke <francisco.deodato@tvfsoftware.com>
2018-02-19 19:46:57 -0300

    added support to post single page

--- single.php
+++ single.php
@@ -19,20 +19,27 @@
   * 
   */
   
-get_header(); ?>
-            <!-- Post -->
-              <section class="post">
-                <header class="major">
-                  <span class="date">April 25, 2017</span>
-                  <h1>This is a<br />
-                  Generic Page</h1>
-                  <p>Aenean ornare velit lacus varius enim ullamcorper proin aliquam<br />
-                  facilisis ante sed etiam magna interdum congue. Lorem ipsum dolor<br />
-                  amet nullam sed etiam veroeros.</p>
-                </header>
-                <div class="image main"><img src="images/pic01.jpg" alt="" /></div>
-                <p>Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum sit amet, fergiat. Pellentesque in mi eu massa lacinia malesuada et a elit. Donec urna ex, lacinia in purus ac, pretium pulvinar mauris. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Curabitur sapien risus, commodo eget turpis at, elementum convallis enim turpis, lorem ipsum dolor sit amet nullam.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus. Donec eget ex magna. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis. Praesent rutrum sem diam, vitae egestas enim auctor sit amet. Pellentesque leo mauris, consectetur id ipsum.</p>
-              </section>
-<?php
+get_header();
+if ( have_posts() ) :
+  while ( have_posts() ) : 
+    the_post();
+  ?>
+    <!-- Post -->
+      <section class="post" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <header class="major">
+          <span class="date"><?php twentyten_posted_on(); ?></span>
+          <h1><?php the_title(); ?></h1>
+          <p>TODO - check what to include here | Aenean ornare velit lacus varius enim ullamcorper proin aliquam<br />
+          facilisis ante sed etiam magna interdum congue. Lorem ipsum dolor<br />
+          amet nullam sed etiam veroeros.</p>
+        </header>
+        <?php 
+          $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full');
+        ?>
+        <div class="image main"><img src="<?php echo esc_url($featured_img_url) ?>" alt="" /></div>
+        <?php the_content(); ?>
+      </section>
+  <?php comments_template( '', true );
+  endwhile;
+endif;
 get_footer();
