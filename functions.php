c1fbab6a540873566385e122f709f8eab2a9f1c4
h3nr1ke <francisco.deodato@tvfsoftware.com>
2018-02-18 23:35:32 -0300

    added calls to css files

--- functions.php
+++ functions.php
@@ -34,11 +34,17 @@ add_image_size( 'appsetc-massively-thumbnail', 448, 273, true );
  * Enqueue all the need files to be included by wordpress engine
  */
 function appsetc_massively_scripts() {
-  // add the theme scripts
+  // add theme scripts
   wp_enqueue_script( 'appsetc-massively-scrollex', get_theme_file_uri( '/assets/js/jquery.scrollex.min.js' ), array( 'jquery' ), '1.0', true );
   wp_enqueue_script( 'appsetc-massively-scrolly', get_theme_file_uri( '/assets/js/jquery.scrolly.min.js' ), array( 'jquery' ), '1.0', true );
   wp_enqueue_script( 'appsetc-massively-skel', get_theme_file_uri( '/assets/js/skel.min.js' ), array( 'jquery' ), '1.0', true );
   wp_enqueue_script( 'appsetc-massively-util', get_theme_file_uri( '/assets/js/util.js' ), array( 'jquery' ), '1.0', true );
   wp_enqueue_script( 'appsetc-massively-main', get_theme_file_uri( '/assets/js/mains.js' ), array( 'jquery' ), '1.0', true );
+
+  //add theme styles
+  wp_enqueue_style( 'style', get_stylesheet_uri() );
+  wp_enqueue_style( 'fontawesome', get_template_directory_uri() . '/assests/css/font-awesome.min.css',false,'1.1','all');
+  wp_enqueue_style( 'noscript', get_template_directory_uri() . '/assests/css/noscript.css',false,'1.1','all');
+  wp_enqueue_style( 'main', get_template_directory_uri() . '/assests/css/main.css',false,'1.1','all');
 }
 add_action( 'wp_enqueue_scripts', 'appsetc_massively_scripts' );
